f35e16a7e4ff7de1bb6ad087030083cbc001a4de
Christian_Alvarado <ChrisD.Alvarado95@gmail.com>
2025-12-01 16:03:22 -0600

    Continuous player movement
    -Player can now move continously
    -Checks for direction change mid-movement added

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -10,10 +10,17 @@ public class PlayerController : MonoBehaviour
     private Tilemap collisionTilemap;
 
     private PlayerActions controls;
+    private WorldDirection currentFacingDirection = WorldDirection.Down;
+
     [SerializeField]
     float moveDuration = 0.3f;
+    [SerializeField]
+    float interactionDelay = 0.3f;
 
-    bool moving = false;
+    bool moveOrdered = false;
+    bool isCurrentlyMoving = false;
+    bool playerIsInteracting = false;
+    bool playerCanInteract = true;
 
     private void Awake()
     {
@@ -33,26 +40,64 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        controls.Main.Movement.performed += ctx => HandleMove(ctx.ReadValue<Vector2>());
+        controls.Main.Movement.started += ctx => HandleMove(ctx.ReadValue<Vector2>());
         controls.Main.Movement.canceled += ctx => HandleMoveCanceled();
+    }
+
+    private void Update()
+    {
+        if (moveOrdered && !isCurrentlyMoving)
+        {
+            if (CanMove(GetFacingDirectionAsVector(currentFacingDirection)))
+            {
+                StartCoroutine(Move(transform.position + (Vector3)GetFacingDirectionAsVector(currentFacingDirection)));
+            }
+        }
     }
 
     private void HandleMove(Vector2 direction)
     {
+        //ignore empty movements
+        if (direction == Vector2.zero) return;
+
+        if (!playerCanInteract) return;
+
+        Debug.Log($"HandleMove. Direction: {(direction)}");
+
+        SetFacingDirection(GetFacingDirection(direction));
+
+
         if (CanMove(direction))
         {
-            moving = true;
-            StartCoroutine(Move(transform.position + (Vector3)direction));
+            moveOrdered = true;
         }
         else
         {
-            moving = false;
+            moveOrdered = false;
         }
+    }
+
+    void SetFacingDirection(WorldDirection direction)
+    {
+        if (direction != WorldDirection.None)
+        {
+            currentFacingDirection = direction;
+        }
+
+        //Animator set facing direction 
+
     }
 
     private void HandleMoveCanceled()
     {
-        moving = false;
+        if(controls.Main.Movement.ReadValue<Vector2>() != Vector2.zero)
+        {
+            Debug.Log("Movement is still ocurring");
+            return;
+        }
+
+        Debug.Log("Handle Move Canceled");
+        moveOrdered = false;
     }
 
     private bool CanMove(Vector2 direction)
@@ -69,6 +114,7 @@ public class PlayerController : MonoBehaviour
 
     IEnumerator Move(Vector2 direction)
     {
+        isCurrentlyMoving = true;
         Vector2 startPosition = transform.position;
         Vector2 endPosition = direction;
 
@@ -82,5 +128,116 @@ public class PlayerController : MonoBehaviour
         }
 
         transform.position = endPosition;
+        isCurrentlyMoving = false;
+
+        if (GetFacingDirectionAsVector(currentFacingDirection) != controls.Main.Movement.ReadValue<Vector2>())
+        {
+            Debug.Log($"Direction changed. New direction:{controls.Main.Movement.ReadValue<Vector2>()}");
+            SetFacingDirection(GetFacingDirection(controls.Main.Movement.ReadValue<Vector2>()));
+        }
+
+        if (!moveOrdered)
+        {
+            StartCoroutine(WaitForMovementEndDelay());
+        }
     }
+
+    IEnumerator WaitForInteractionDelay()
+    {
+        while (isCurrentlyMoving)
+        {
+            yield return null;
+        }
+
+        playerIsInteracting = true;
+
+        float elapsedTime = 0;
+        while (elapsedTime < interactionDelay)
+        {
+            elapsedTime += Time.deltaTime;
+            float percent = elapsedTime / interactionDelay;
+            yield return null;
+        }
+
+        playerIsInteracting = false;
+    }
+
+    IEnumerator WaitForMovementEndDelay()
+    {
+        while (isCurrentlyMoving)
+        {
+            yield return null;
+        }
+
+        if (controls.Main.Movement.ReadValue<Vector2>() != Vector2.zero)
+        {
+            yield break;
+        }
+
+        playerCanInteract = false;
+
+        float elapsedTime = 0;
+        while (elapsedTime < interactionDelay)
+        {
+            elapsedTime += Time.deltaTime;
+            float percent = elapsedTime / interactionDelay;
+            yield return null;
+        }
+
+        playerCanInteract = true;
+
+
+        if (controls.Main.Movement.ReadValue<Vector2>() != Vector2.zero)
+        {
+            moveOrdered = true;
+            SetFacingDirection(GetFacingDirection(controls.Main.Movement.ReadValue<Vector2>()));
+        }
+    }
+
+
+
+    WorldDirection GetFacingDirection(Vector2 direction)
+    {
+        return direction switch
+        {
+            _ when direction == new Vector2(1, 0) => WorldDirection.Right,
+            _ when direction == new Vector2(0, 1) => WorldDirection.Up,
+            _ when direction == new Vector2(-1, 0) => WorldDirection.Left,
+            _ when direction == new Vector2(0, -1) => WorldDirection.Down,
+
+            //In case something goes wrong:
+            _ when direction == new Vector2(0, 0) => WorldDirection.None,
+        };
+    }
+
+    Vector2 GetFacingDirectionAsVector(WorldDirection direction)
+    {
+        switch (direction)
+        {
+            case WorldDirection.Up:
+                return new Vector2(0, 1);
+
+            case WorldDirection.Down:
+                return new Vector2(0, -1);
+
+            case WorldDirection.Right:
+                return new Vector2(1, 0);
+
+            case WorldDirection.Left:
+                return new Vector2(-1, 0);
+
+            case WorldDirection.None:
+            default:
+                return new Vector2(0, 0);
+        }
+    }
+}
+
+public enum WorldDirection
+{
+    Up,
+    Down,
+    Right,
+    Left,
+    None
 }
