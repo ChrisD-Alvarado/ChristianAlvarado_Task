d6dc63ca19952c143ad826fc8de2b45177263b30
Christian_Alvarado <ChrisD.Alvarado95@gmail.com>
2025-12-02 00:43:47 -0600

    Pick Up Interactable functions

--- Assets/Scripts/Interactables/PickUpItemInteractable.cs
+++ Assets/Scripts/Interactables/PickUpItemInteractable.cs
@@ -6,15 +6,59 @@ public class PickUpItemInteractable : Interactable
     ItemDataScriptableObject itemData;
 
     [SerializeField]
+    SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    bool spawnInAwake = false;
+
+    bool spawnedInMap;
+
+    [SerializeField]
     int quantity;
 
     private void Awake()
     {
         kind = InteractableKind.PickableItem;
+        if (spawnInAwake)
+        {
+            SpawnOrAdd(itemData, quantity);
+        }
     }
 
     public override void Interact()
     {
+        //Make sure item is actually detectable
+        if (state != InteractableState.Detectable)
+        {
+            return;
+        }
 
+        PlayerController player = FindAnyObjectByType<PlayerController>();
+        player.GrantItem(itemData, quantity);
+
+        if (hidesOnFirstInteraction)
+        {
+            HideItem();
+        }
+    }
+
+    public void SpawnOrAdd(ItemDataScriptableObject item, int amount)
+    {
+        if (!spawnedInMap)
+        {
+            spawnedInMap = true;
+            spriteRenderer.enabled = true;
+            quantity = amount;
+        }
+        else
+        {
+            quantity += amount;
+        }
+    }
+
+    void HideItem()
+    {
+        spriteRenderer.enabled = false;
+        state = InteractableState.Hidden;
     }
 }
